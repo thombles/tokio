f803e903759684c6aa5c41b5a0b4898819ba5706
Thomas Karpiniec <tom@ditto.live>
2019-07-09 12:43:29 +1000

    tcp: test that poll_accept errors when out of fds

--- tokio-tcp/tests/tcp.rs
+++ tokio-tcp/tests/tcp.rs
@@ -1,5 +1,6 @@
 extern crate env_logger;
 extern crate futures;
+extern crate libc;
 extern crate mio;
 extern crate net2;
 extern crate tokio_io;
@@ -134,5 +135,74 @@ mod linux {
         assert_eq!(b"hello world", &buf[..]);
 
         t.join().unwrap();
+    }
+}
+
+#[cfg(unix)]
+mod unix {
+    use futures::future;
+    use futures::{Async, Future};
+    use libc::{connect, dup, sockaddr, sockaddr_in, socket, AF_INET, EMFILE, SOCK_STREAM};
+    use std::io::Error;
+    use std::mem;
+    use std::net::SocketAddr;
+    use tokio_tcp::TcpListener;
+
+    #[test]
+    fn listen_failure() {
+        future::lazy(|| {
+            // Set up a tokio TCP listener
+            let addr = t!("127.0.0.1:0".parse::<SocketAddr>());
+            let mut srv = t!(TcpListener::bind(&addr));
+            let port = t!(srv.local_addr()).port();
+
+            // Poll once to set up the reactor before we use all the FDs
+            match srv.poll_accept() {
+                Ok(Async::NotReady) => (),
+                _ => panic!("unexpected poll result"),
+            };
+
+            // Create socket and consume all FDs allowed by this process
+            let sock = unsafe { socket(AF_INET, SOCK_STREAM, 0) };
+            assert!(sock != -1);
+            loop {
+                if unsafe { dup(sock) } == -1 {
+                    let err = Error::last_os_error();
+                    assert_eq!(err.raw_os_error(), Some(EMFILE));
+                    break;
+                }
+            }
+
+            // The listener should still be okay so far
+            match srv.poll_accept() {
+                Ok(Async::NotReady) => (),
+                _ => panic!("unexpected poll result"),
+            };
+
+            // Use the original socket to connect
+            let sa_len = mem::size_of::<sockaddr_in>() as u32;
+            let sa = sockaddr_in {
+                sin_family: AF_INET as u8,
+                sin_port: port.to_be(),
+                sin_addr: libc::in_addr {
+                    s_addr: 0x7f000001_u32.to_be(), // 127.0.0.1
+                },
+                sin_zero: [0; 8],
+                sin_len: sa_len as u8,
+            };
+
+            // This will succeed, but we can't accept it
+            let addr_ptr: *const sockaddr = unsafe { mem::transmute(&sa) };
+            let connect_result = unsafe { connect(sock, addr_ptr, sa_len) };
+            assert_eq!(connect_result, 0);
+
+            // There is no FD available to represent an incoming connection
+            let res = future::poll_fn(|| srv.poll_accept()).wait();
+            assert!(res.is_err());
+
+            future::ok::<(), ()>(())
+        })
+        .wait()
+        .unwrap();
     }
 }
